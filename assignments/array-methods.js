35c56c33570c77155c6ae60916ae2ee26a8a7f49
spenclark <spenclark@hotmail.com>
2019-08-21 13:02:55 -0700

    Finishing commit

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,28 +58,55 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
+
+runners.forEach(function(element){
+
+  return fullNames.push(`${element.first_name} ${element.last_name}`);
+ 
+});
 console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
-let firstNamesAllCaps = [];
+
+let firstNamesAllCaps = runners.map(runners =>{ return runners.first_name.toUpperCase()});
+
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
-let runnersLargeSizeShirt = [];
+let runnersLargeSizeShirt = runners.filter(runners =>{ return runners.shirt_size === 'L'});
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = 0;
+let ticketPriceTotal = runners.reduce( (total, runner) =>{ return total += runner.donation }, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 Student discount - filter all runners with a .edu email
+let emaillist = [];
 
+runners.forEach(function(name){
+    return emaillist.push(`${name.email}`)
+});
+// need help with if statement
+// let studentdiscount = [];
+
+// emaillist.forEach(function(search){
+//   if ()
+//   return studentdiscount.push();
+// });
+
+console.log(emaillist)
 // Problem 2
+let LastNamesAllCaps = runners.map(runners =>{ return runners.last_name.toUpperCase()});
 
+console.log(LastNamesAllCaps);
 // Problem 3
+
+let highID = runners.filter( runner => {return runner.id > 25});
+
+console.log(highID);
--- assignments/closure.js
+++ assignments/closure.js
@@ -4,19 +4,23 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+var me = 'Spencer'
+function greetMe(){
+  console.log(`Hello, my name is ${me}`)
+}
+greetMe()
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
-const counterMaker = () => {
-  // IMPLEMENTATION OF counterMaker:
-  // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  // 2- Declare a function `counter`. It should increment and return `count`.
-  //      NOTE: This `counter` function, being nested inside `counterMaker`,
-  //      "closes over" the `count` variable. It can "see" it in the parent scope!
-  // 3- Return the `counter` function.
-};
+// let count = 0;
+// const counterMaker = () => {
+ 
+//     function counter(){
+//     return count;
+//   };
+// };
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
 // myCounter(); // 2
